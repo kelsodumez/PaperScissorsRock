ce202498f81b18999f9e7f3248e90e33de223920
kelsodumez <17232@burnside.school.nz>
2021-08-17 14:33:31 +1200

    1.1.1 - getting modal boxes working

--- static/js/psr.js
+++ static/js/psr.js
@@ -1,20 +1,13 @@
 const socket = io.connect('http://127.0.0.1:5000');
 
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+
 window.addEventListener('load', () => {
     console.log("reached here") // debug
     socket.emit('join')
 });
-
-// document.querySelector('#join').addEventListener('click', () => {
-//     const id = +window.location.href.split('\/').pop()
-//     socket.emit('join', {'room':id})
-// });
-
-// document.getElementById('send-action').addEventListener('click', () => {
-//     const choice = "rock"
-//     console.log(choice)
-//     socket.emit('sendAction', {'choice': choice, 'room': room})
-// });
 
 document.getElementById('user-selection').addEventListener('click', () => {
     console.log("wowowowo")
