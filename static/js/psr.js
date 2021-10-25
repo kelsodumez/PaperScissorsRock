e9f504fc61a10f6c83f220207997c45896f39915
kelsodumez <17232@burnside.school.nz>
2021-10-25 16:08:59 +1300

    1.7.3 - work on making a modal that displays win/loss

--- static/js/psr.js
+++ static/js/psr.js
@@ -1,8 +1,7 @@
 const socket = io.connect('http://127.0.0.1:5000');
 
 var response_modal = document.getElementById("responseModal");
-var win_modal = document.getElementById("winModal")
-var loss_modal = document.getElementById("lossModal")
+var result_modal = document.getElementById("resultModal")
 var span = document.getElementsByClassName("close")[0];  
 var currentUser = $('#user-data').data(); // due to the way sessions work with socketio it is neccessary to send the current user from js to socketio
 let challenger;
@@ -38,24 +37,27 @@ socket.on('broadcast-choice', function(data) {
     document.getElementById("name").innerHTML = challenger.user;
 });
 
-socket.on('broadcast-result', function(data) {
-
-});
-
 document.getElementById('user-response-rock').addEventListener('click', () => {
-    console.log(1, challenger)
     socket.emit('sendResponse', {'challenger': challenger, 'move': 'rock'})
     response_modal.style.display = "none";
 });
 
 document.getElementById('user-response-paper').addEventListener('click', () => {
-    console.log(2, challenger)
     socket.emit('sendResponse', {'challenger': challenger, 'move': 'paper'})
     response_modal.style.display = "none";
 });
 
 document.getElementById('user-response-scissors').addEventListener('click', () => {
-    console.log(3, challenger)
     socket.emit('sendResponse', {'challenger': challenger, 'move': 'scissors'})
     response_modal.style.display = "none";
+});
+
+socket.on('broadcast-result', function(data) {
+    if (data == "win") {
+        document.getElementById("result").innerHTML = "Win";
+    }
+    else if (data == "loss") {
+        document.getElementById("result").innerHTML = "Lose";
+    }
+    win_modal.style.display = "block";   
 });
